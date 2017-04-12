4fec052ee3c840aae0fd0dbc9c3eeab8eb67e7ec
Thomas Röggla <tom@lichtfragmente.com>
2017-04-12 17:50:01 +0200

    fixed issue in __repr__() method of the User model

--- app/models/user.py
+++ app/models/user.py
@@ -7,4 +7,8 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
 
     def __repr__(self):
-        return "<User id=%d, email=%s>" % (self.name, self.email)
+        return "<User id=%d, name=%s, email=%s>" % (
+            self.id,
+            self.name,
+            self.email
+        )
