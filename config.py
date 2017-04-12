7f68d64669c1ed7710901cea3a8ba56455a4be3b
Thomas Röggla <tom@lichtfragmente.com>
2017-04-12 16:24:16 +0200

    added config for running DB migrations

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 SQLALCHEMY_DATABASE_URI = "postgresql://docker@db:5432/2immerse_editor"
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "migrations")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
