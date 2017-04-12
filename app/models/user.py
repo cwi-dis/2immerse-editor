5745a1c5fa9640841670d3e8ae0926a6de0f6f73
Thomas Röggla <tom@lichtfragmente.com>
2017-04-12 16:25:16 +0200

    added first draft of user model

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+
+    def __repr__(self):
+        return "<User id=%d, email=%s>" % (self.name, self.email)
