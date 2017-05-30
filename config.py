4c8a99ffd5ba46bab4b9f8b61ddda0b1f446e2a5
Thomas Röggla <tom@lichtfragmente.com>
2017-05-30 14:39:01 +0200

    removed config related to the database

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = "postgresql://docker@db:5432/2immerse_editor"
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "migrations")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
