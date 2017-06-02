77293ffb5dedb45ce4e608c76d0ecaf8315f691a
Thomas Röggla <tom@lichtfragmente.com>
2017-06-02 14:41:32 +0200

    added stub components for master manager and program author

---
+++ app/static/js/components/master_manager.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class MasterManager extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div>Manage Masters</div>
+    );
+  }
+}
+
+export default MasterManager;
---
+++ app/static/js/components/program_author.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class ProgramAuthor extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div>Author Program</div>
+    );
+  }
+}
+
+export default ProgramAuthor;
