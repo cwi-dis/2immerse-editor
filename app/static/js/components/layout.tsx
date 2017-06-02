64c32109e550d10358553c65c96e8008671c2ced
Thomas Röggla <tom@lichtfragmente.com>
2017-06-02 14:42:22 +0200

    added links to layout component

--- app/static/js/components/layout.tsx
+++ app/static/js/components/layout.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
+import { Link } from "react-router";
 
 class Layout extends React.Component<{children?: any}, {}> {
   public render() {
     return (
       <div>
-        <div>
-          <ul>
-            <li>Design Layout</li>
-            <li>Manage Masters</li>
-            <li>Author Program</li>
+        <div style={{width: "100%", height: 50}}>
+          <ul style={{listStyleType: "none"}}>
+            <li><Link to="/">Design Layout</Link></li>
+            <li><Link to="/masters">Manage Masters</Link></li>
+            <li><Link to="/program">Author Program</Link></li>
           </ul>
         </div>
         <div>{React.cloneElement(this.props.children, Object.assign({}, this.props))}</div>
