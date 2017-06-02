70a0be6ae3d0b84d88c71132c6377bc0773e15bc
Thomas Röggla <tom@lichtfragmente.com>
2017-06-02 13:51:07 +0200

    added stub component for layout designer

---
+++ app/static/js/components/layout_designer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class LayoutDesigner extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div>Layout Designer</div>
+    );
+  }
+}
+
+export default LayoutDesigner;
