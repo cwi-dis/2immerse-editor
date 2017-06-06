6d44e3bf48e3091087f8258894db9d8e6527c11b
Thomas Röggla <tom@lichtfragmente.com>
2017-06-06 17:38:00 +0200

    styled buttons in layout designer and added border to menu

--- app/static/js/components/layout_designer/layout_designer.tsx
+++ app/static/js/components/layout_designer/layout_designer.tsx
@@ -11,12 +11,14 @@ class LayoutDesigner extends React.Component<LayoutDesignerProps, {}> {
       <div className="column">
         <div className="content">
           <h1>Layout Designer</h1>
-          <div className="field">
-            <p className="control">
-              <button onClick={this.props.addCommunalDevice}>Add communal device</button>
-            </p>
+
+          <div className="block">
+            <a style={{marginRight: 10}} className="button is-info" onClick={this.props.addCommunalDevice}>Add communal device</a>
+            <a className="button is-info" onClick={this.props.addPersonalDevice}>Add personal device</a>
           </div>
+
           <br/>
+
           <div className="columns">
             <div className="column is-8" style={{borderRight: "1px solid #B1B1B1"}}>
               <h3 style={{textAlign: "center"}}>Communal Device</h3>
