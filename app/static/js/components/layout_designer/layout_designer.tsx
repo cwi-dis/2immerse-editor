f8b76350d8dfc3cea0b448b627af84065cf1c49a
Thomas Röggla <tom@lichtfragmente.com>
2017-06-07 02:07:08 +0200

    rendering screens part of state tree in layout designer

--- app/static/js/components/layout_designer/layout_designer.tsx
+++ app/static/js/components/layout_designer/layout_designer.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 
+import { ApplicationState } from "../../store";
+
 interface LayoutDesignerProps {
   addPersonalDevice: () => void;
   addCommunalDevice: () => void;
 }
 
-class LayoutDesigner extends React.Component<LayoutDesignerProps, {}> {
+class LayoutDesigner extends React.Component<ApplicationState & LayoutDesignerProps, {}> {
   public render() {
     return (
       <div className="column">
@@ -21,10 +23,12 @@ class LayoutDesigner extends React.Component<LayoutDesignerProps, {}> {
 
           <div className="columns">
             <div className="column is-8" style={{borderRight: "1px solid #B1B1B1"}}>
-              <h3 style={{textAlign: "center"}}>Communal Device</h3>
+              <h3 style={{textAlign: "center"}}>Communal Device ({this.props.screens.communalScreens.length})</h3>
+              <p>{this.props.screens.communalScreens.map((screen, i) => <span key={i}>{screen}, </span>)}</p>
             </div>
             <div className="column is-4">
-              <h3 style={{textAlign: "center"}}>Personal Devices</h3>
+              <h3 style={{textAlign: "center"}}>Personal Devices ({this.props.screens.personalScreens.length})</h3>
+              <p>{this.props.screens.personalScreens.map((screen, i) => <span key={i}>{screen}, </span>)}</p>
             </div>
           </div>
         </div>
