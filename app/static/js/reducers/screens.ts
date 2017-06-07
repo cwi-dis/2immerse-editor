beb09181ef0829df5cfba7d7252ed218bcc46389
Thomas Röggla <t.roggla@cwi.nl>
2017-06-07 17:40:11 +0200

    introduced proper type defintion for screens and screen regions

--- app/static/js/reducers/screens.ts
+++ app/static/js/reducers/screens.ts
@@ -3,17 +3,32 @@ import { List } from "immutable";
 import { Action, PayloadAction } from "../actions";
 import { ApplicationState } from "../store";
 
+interface ScreenRegion {
+  dividerPosition: number;
+  orientation: "horizontal" | "vertical";
+  leftSubRegion?: ScreenRegion;
+  rightSubRegion?: ScreenRegion;
+}
+
+interface Screen {
+  name: string;
+  orientation: "portrait" | "landscape";
+  regions?: ScreenRegion;
+}
+
 export interface ScreenState {
-  personalScreens: List<string>;
-  communalScreens: List<string>;
+  personalScreens: List<Screen>;
+  communalScreens: List<Screen>;
 }
 
 const defaultState: ScreenState = {
-  personalScreens: List<string>(),
-  communalScreens: List<string>()
+  personalScreens: List<Screen>(),
+  communalScreens: List<Screen>()
 };
 
 function getRandomInt(min: number = 0, max: number = 10) {
+  const blag = List<string>(["a", "b", "c"]);
+
   min = Math.ceil(min);
   max = Math.floor(max);
 
@@ -24,22 +39,34 @@ function screens(state: ScreenState = defaultState, action: Action): ScreenState
   console.log("Action triggered:");
   console.log(action);
 
+  state.personalScreens.map(screen => screen.name);
+
   switch (action.type) {
-    case "ADD_PERSONAL_DEVICE":
+    case "ADD_PERSONAL_DEVICE": {
       console.log("personal device reducer called");
 
-      return {
-        ...state,
-        personalScreens: state.personalScreens.push("personal " + getRandomInt())
+      const screen: Screen = {
+        name: "personal " + getRandomInt(),
+        orientation: "portrait"
       };
-    case "ADD_COMMUNAL_DEVICE":
-      console.log("communal device reducer called");
 
       return {
         ...state,
-        communalScreens: state.communalScreens.push("communal " + getRandomInt())
+        personalScreens: state.personalScreens.push(screen)
       };
-    case "REMOVE_PERSONAL_DEVICE": {
+    } case "ADD_COMMUNAL_DEVICE": {
+      console.log("communal device reducer called");
+
+      const screen: Screen = {
+        name: "communal " + getRandomInt(),
+        orientation: "landscape"
+      };
+
+      return {
+        ...state,
+        communalScreens: state.communalScreens.push(screen)
+      };
+    } case "REMOVE_PERSONAL_DEVICE": {
       const { id } = (action as PayloadAction<{id: number}>).payload;
 
       return {
