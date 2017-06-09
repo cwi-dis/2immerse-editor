61f10c12ece312404c475bc502db710151798db6
Jack Jansen <Jack.Jansen@cwi.nl>
2017-06-09 09:51:38 +0200

    Baby steps.

---
+++ app/api/__init__.py
@@ -0,0 +1,9 @@
+
+class API:
+    def __init__(self):
+        pass
+        
+    def dump(self):
+        return 'Nothing to show yet'
+        
+api = API()
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from app import app
-
+from api import api
 
 @app.route("/")
 def main():
     return render_template("main.html")
+
+@app.route("/api/v1/<verb>")
+def call_api(verb):
+    func = getattr(api, verb)
+    return func()
+    
