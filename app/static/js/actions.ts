138337f8fae54874d3f7e94d6569b94e1a149008
Thomas Röggla <t.roggla@cwi.nl>
2017-06-12 14:47:16 +0200

    redesigned types for actions to be able to use discriminated unions

--- app/static/js/actions.ts
+++ app/static/js/actions.ts
@@ -1,15 +1,12 @@
 import { ActionCreatorsMapObject } from "redux";
 
-export interface Action {
-  type: string;
-};
-
-export interface PayloadAction<T> extends Action {
+export interface PayloadAction<U, T> {
+  type: U;
   payload: T;
 }
 
-export type ADD_DEVICE = {type: "personal" | "communal"}
-function addDevice(type: "personal" | "communal"): PayloadAction<ADD_DEVICE> {
+type ADD_DEVICE = PayloadAction<"ADD_DEVICE", {type: "personal" | "communal"}>
+function addDevice(type: "personal" | "communal"): ADD_DEVICE {
   return {
     type: "ADD_DEVICE",
     payload: {
@@ -18,8 +15,8 @@ function addDevice(type: "personal" | "communal"): PayloadAction<ADD_DEVICE> {
   };
 }
 
-export type REMOVE_DEVICE = {id: string};
-function removeDevice(id: string): PayloadAction<REMOVE_DEVICE> {
+type REMOVE_DEVICE = PayloadAction<"REMOVE_DEVICE", {id: string}>
+function removeDevice(id: string): REMOVE_DEVICE {
   return {
     type: "REMOVE_DEVICE",
     payload: {
@@ -28,8 +25,8 @@ function removeDevice(id: string): PayloadAction<REMOVE_DEVICE> {
   };
 }
 
-export type SPLIT_REGION = {screenId: string, regionId: string, orientation: "horizontal" | "vertical", position: number};
-function splitRegion(screenId: string, regionId: string, orientation: "horizontal" | "vertical", position: number): PayloadAction<SPLIT_REGION> {
+type SPLIT_REGION = PayloadAction<"SPLIT_REGION", {screenId: string, regionId: string, orientation: "horizontal" | "vertical", position: number}>;
+function splitRegion(screenId: string, regionId: string, orientation: "horizontal" | "vertical", position: number): SPLIT_REGION {
   return {
     type: "SPLIT_REGION",
     payload: {
@@ -40,6 +37,8 @@ function splitRegion(screenId: string, regionId: string, orientation: "horizonta
     }
   };
 }
+
+export type Action = ADD_DEVICE | REMOVE_DEVICE | SPLIT_REGION;
 
 export const actionCreators: ActionCreatorsMapObject = {
   addDevice,
