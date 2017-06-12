ea0654f2739f96a1bbb25fc236377d301700675d
Thomas Röggla <tom@lichtfragmente.com>
2017-06-13 00:34:16 +0200

    introduced ActionHandler class to express reducers in a more functional way

--- app/static/js/util.ts
+++ app/static/js/util.ts
@@ -11,3 +11,31 @@ export function getRandomInt(min: number = 0, max: number = 10) {
 
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+type ActionHandlerFunction<T> = (state: T, action: Action) => T;
+
+export class ActionHandler<T> {
+  private initialState: T;
+  private handlers: {[key: string]: ActionHandlerFunction<T>};
+
+  constructor(initialState: T) {
+    this.initialState = initialState;
+    this.handlers = {};
+  }
+
+  public addHandler(action: string, fn: ActionHandlerFunction<T>) {
+    this.handlers[action] = fn;
+  }
+
+  public getReducer(): ActionHandlerFunction<T> {
+    return (state: T = this.initialState, action: Action) => {
+      for (const key in this.handlers) {
+        if (key === action.type) {
+          return this.handlers[key](state, action);
+        }
+      }
+
+      return state;
+    };
+  }
+}
