6134cb579fe92e3c9206a61121b3389e60c17b54
Thomas Röggla <tom@lichtfragmente.com>
2017-07-11 18:35:22 +0200

    maintaining acces path for each node in the chapter tree

--- app/static/js/components/program_author.tsx
+++ app/static/js/components/program_author.tsx
@@ -12,25 +12,26 @@ class ProgramAuthor extends React.Component<ApplicationState, {}> {
   private boxMargin: [number, number] = [20, 20];
   private boxHotArea = 20;
 
-  private handleBoxClick(chapterId: string, topLeft: [number, number], size: [number, number]) {
+  private handleBoxClick(accessPath: Array<number>, topLeft: [number, number], size: [number, number]) {
     const bottomRight = [topLeft[0] + size[0], topLeft[1] + size[1]];
     const {x, y} = this.stage.getStage().getPointerPosition();
 
     if (x <= topLeft[0] + this.boxHotArea) {
-      console.log("ADD BEFORE", chapterId);
+      console.log("ADD BEFORE", accessPath);
     } else if (x >= bottomRight[0] - this.boxHotArea) {
-      console.log("ADD AFTER", chapterId);
+      console.log("ADD AFTER", accessPath);
     } else if (y >= bottomRight[1] - this.boxHotArea) {
-      console.log("ADD CHILD TO", chapterId);
+      console.log("ADD CHILD TO", accessPath);
     }
   }
 
-  private drawChapters(chapters: List<Chapter>, startPos = [10, 10]): Array<any> {
+  private drawChapters(chapters: List<Chapter>, startPos = [10, 10], accessPath: Array<number> = []): Array<any> {
     return chapters.reduce((result: any[], chapter, i) => {
       const [x, y] = startPos;
 
       const leafNodes = countLeafNodes(chapter);
       const boxWidth = leafNodes * this.baseBoxSize[0] + (leafNodes - 1) * this.boxMargin[0];
+      const currentPath = accessPath.concat(i);
 
       let rect = [
         <Rect key={chapter.id}
@@ -38,12 +39,13 @@ class ProgramAuthor extends React.Component<ApplicationState, {}> {
               x={x} y={y}
               onMouseEnter={() => this.stage.getStage().container().style.cursor = "pointer" }
               onMouseLeave={() => this.stage.getStage().container().style.cursor = "default" }
-              onClick={this.handleBoxClick.bind(this, chapter.id, [x, y], [boxWidth, this.baseBoxSize[1]])}
+              onClick={this.handleBoxClick.bind(this, currentPath, [x, y], [boxWidth, this.baseBoxSize[1]])}
               height={this.baseBoxSize[1]} width={boxWidth} />
       ].concat(
         this.drawChapters(
           chapter.children,
           [x, y + this.baseBoxSize[1] + this.boxMargin[1]],
+          currentPath
         )
       );
       startPos[0] += boxWidth + this.boxMargin[0];
