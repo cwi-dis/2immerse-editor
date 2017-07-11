75a03eb93ac377a5cc4d74bd9fe9462a3f7a4c5a
Thomas Röggla <tom@lichtfragmente.com>
2017-07-11 21:48:19 +0200

    added reducer for inserting a chapter after another

--- app/static/js/reducers/chapters.ts
+++ app/static/js/reducers/chapters.ts
@@ -65,4 +65,28 @@ actionHandler.addHandler("ADD_CHAPTER_BEFORE", (state, action: ADD_CHAPTER_BEFOR
   return state.updateIn(keyPath, () => updatedChildren);
 });
 
+actionHandler.addHandler("ADD_CHAPTER_AFTER", (state, action: ADD_CHAPTER_AFTER) => {
+  const { accessPath } = action.payload;
+
+  const insertIndex = accessPath[accessPath.length - 1];
+  let list: List<Chapter> = state;
+
+  accessPath.slice(0, accessPath.length - 1).forEach((i) => {
+    list = (list.get(i)!.get("children") as List<Chapter>);
+  });
+
+  const newChapter = Map({
+    id: shortid.generate(),
+    masterLayouts: List(),
+    children: List()
+  });
+  const updatedChildren = (insertIndex >= list.count()) ? list.push(newChapter) : list.insert(insertIndex + 1, newChapter);
+
+  const keyPath = List(accessPath.slice(0, accessPath.length - 1).reduce((path: Array<string | number>, i) => {
+    return path.concat([i, "children"]);
+  }, []));
+
+  return state.updateIn(keyPath, () => updatedChildren);
+});
+
 export default actionHandler.getReducer();
