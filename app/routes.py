1f195ffbdcbff3eb6ea192f0dcf658ed72e3e3a0
Thomas Röggla <t.roggla@cwi.nl>
2017-07-18 13:45:59 +0200

    changed editor url to /editor and root to render the landing page

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,12 @@ from flask import render_template, abort
 from app import app
 import api.routes
 
-@app.route("/")
-def main():
-    return render_template("main.html")
 
+@app.route("/")
+def landing_page():
+    return render_template("landing_page.html")
+
+
+@app.route("/editor")
+def editor():
+    return render_template("editor.html")
