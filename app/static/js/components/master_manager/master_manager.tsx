35d83ad6054150c6672e082f4f058e58ca58aca2
Thomas Röggla <tom@lichtfragmente.com>
2017-07-19 14:15:16 +0200

    added functionality to add new masters to MasterManager component

--- app/static/js/components/master_manager/master_manager.tsx
+++ app/static/js/components/master_manager/master_manager.tsx
@@ -1,15 +1,41 @@
 import * as React from "react";
 
-class MasterManager extends React.Component<{}, {}> {
+import { ApplicationState } from "../../store";
+import { MasterActions } from "../../actions/masters";
+
+class MasterManager extends React.Component<ApplicationState & MasterActions, {}> {
+  private addMaster() {
+    const masterName = prompt("Master layout name:");
+
+    if (masterName !== null && masterName !== "") {
+      this.props.addMasterLayout(masterName);
+    }
+  }
+
   public render() {
     return (
       <div className="columnlayout">
         <div className="content" style={{flexGrow: 1}}>
           <h3>Manage Masters</h3>
+          <p>Move along, nothing to see here yet!</p>
         </div>
         <div className="sidebar">
           <div className="content">
-            <h3>Sidebar</h3>
+            <button style={{width: "100%"}}
+                    className="button is-info"
+                    onClick={this.addMaster.bind(this)}>
+              Add new master layout
+            </button>
+
+            <div style={{marginTop: 30, color: "#E2E2E2"}}>
+              {this.props.masters.map((master, i) => {
+                return (
+                  <div style={{width: "100%", marginTop: 10, paddingBottom: 10, borderBottom: "1px solid #555555", cursor: "pointer"}}>
+                    {i + 1}. {master.name}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
