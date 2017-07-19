8b027ba89fec8a446d09de2a7895f7fe1413786c
Thomas Röggla <tom@lichtfragmente.com>
2017-07-19 14:54:01 +0200

    added function to hash the contents of a file to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
 from flask import render_template, abort
+from hashlib import sha256
+
 from app import app
 import api.routes
+
+
+def hash_file(path):
+    h = sha256()
+
+    with open(path, 'rb') as f:
+        while True:
+            data = f.read(2**16)
+
+            if not data:
+                break
+
+            h.update(data)
+
+    return h.hexdigest()
 
 
 @app.route("/")
