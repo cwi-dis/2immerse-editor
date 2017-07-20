18fc05e53e9cbb04f0608645b97b1491c76725e0
Jack Jansen <Jack.Jansen@cwi.nl>
2017-07-20 17:17:39 +0200

    Add helper to get around CORS issues

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,6 +3,18 @@ from api import api
 from flask import Response, request, abort
 import json
 
+#
+# Disable CORS problems
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+app.after_request(add_cors_headers)
+
 API_ROOT = '/api/v1'
 
 #
