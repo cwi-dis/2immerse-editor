5505df746246640bb12900a180c03c9b1b9231f5
Thomas Röggla <tom@lichtfragmente.com>
2017-07-21 08:07:18 +0200

    properly layed out dmappc containers in the sidebar

--- app/static/js/components/master_manager/dmappc_container.tsx
+++ app/static/js/components/master_manager/dmappc_container.tsx
@@ -6,12 +6,13 @@ class DMAppcContainer extends React.Component<{}, {}> {
       <div style={{height: "50%", borderTop: "1px solid #161616", overflow: "scroll", padding: 5, display: "flex", flexWrap: "wrap", alignContent: "flex-start"}}>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i: number) => {
           return (
-            <div key={i} draggable style={{backgroundColor: "#555555", margin: 5, width: 133, height: 120, padding: 5}}>
-              <b>DMAppC {i}</b>
-              <p>This is a short description</p>
+            <div key={i} draggable style={{backgroundColor: "#262626", margin: 3, width: 140, height: 140, padding: 5, float: "left"}}>
+              <b style={{fontSize: 15}}>DMAppC {i}</b>
+              <p style={{marginTop: 10, fontSize: 12}}>This is a short description</p>
             </div>
           );
         })}
+        <br style={{clear: "both"}} />
       </div>
     );
   }
