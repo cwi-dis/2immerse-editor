ee9fc2df4b363d295df922d2686b0cebbad390cb
Thomas Röggla <tom@lichtfragmente.com>
2017-07-23 15:11:16 +0200

    new documents can now also be created via file upload

--- app/api/api.py
+++ app/api/api.py
@@ -2,26 +2,36 @@ from flask import jsonify, Response, request
 import uuid
 import document
 
+
 class API:
     def __init__(self):
         self.documents = {}
-        
+
     def dump(self):
         rv = '%d documents\n\n' % len(self.documents)
+
         for k in self.documents:
             rv += '%s:\n%s\n' % (k, self.documents[k].dump())
+
         return rv
-        
+
     def document(self):
         if request.method == 'POST':
             documentId = uuid.uuid4()
             doc = document.Document()
-            if request.data:
+
+            if request.files and request.files["document"]:
+                docstream = request.files["document"].stream
+                doc.loadXml(docstream.read())
+            elif request.data:
                 doc.loadXml(request.data)
             elif 'url' in request.args:
                 doc.load(request.args['url'])
+
             self.documents[documentId] = doc
+
             return jsonify(documentId=documentId)
+
         return jsonify(self.documents.keys())
-        
+
 api = API()
