c878110f376e71721b16527c643c1e886004e780
Thomas Röggla <tom@lichtfragmente.com>
2017-07-23 14:41:30 +0200

    added route for launching the trigger tool and updated link

--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,30 @@ from util import hash_file, get_head_revision
 import api.routes
 
 
-BUNDLE_HASH = hash_file("./app/static/dist/bundle.js")
+EDITOR_HASH = hash_file("./app/static/dist/editor.js")
+TRIGGER_HASH = hash_file("./app/static/dist/trigger.js")
 LANDINGPAGE_HASH = hash_file("./app/static/dist/landing_page.js")
 
 
 @app.route("/")
 def landing_page():
-    return render_template("landing_page.html", key=BUNDLE_HASH)
+    return render_template("landing_page.html", key=LANDINGPAGE_HASH)
 
 
 @app.route("/editor")
 def editor():
-    return render_template("editor.html", key=LANDINGPAGE_HASH)
+    return render_template(
+        "main.html",
+        filename="editor.js", key=EDITOR_HASH
+    )
+
+
+@app.route("/trigger")
+def trigger():
+    return render_template(
+        "main.html",
+        filename="trigger.js", key=TRIGGER_HASH
+    )
 
 
 @app.route("/version")
