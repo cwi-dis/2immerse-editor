a68d7542cae61db6bfca3db4a27c93561eb2549c
Thomas Röggla <tom@lichtfragmente.com>
2017-08-02 12:51:28 +0200

    applying redux-thunk middleware to the store

--- app/static/js/editor/store.ts
+++ app/static/js/editor/store.ts
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import { syncHistoryWithStore } from "react-router-redux";
 import { hashHistory } from "react-router";
+import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/index";
 import { ChapterState } from "./reducers/chapters";
@@ -13,9 +14,11 @@ export interface ApplicationState {
   screens: ScreenState;
 };
 
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export const history = syncHistoryWithStore(hashHistory, store);
