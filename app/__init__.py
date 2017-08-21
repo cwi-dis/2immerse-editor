ab519a11e0889f2cea1be8b3f4987ef5bf90f083
Jack Jansen <Jack.Jansen@cwi.nl>
2017-08-21 22:42:01 +0200

    Attempting to print a version number at startup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
+import sys
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object("config")
 
 from app import routes
+
+import util
+
+def print_version(out=sys.stderr):
+    branch = 'unknown'
+    revision = 'unknown'
+    try:
+        branch = util.get_current_branch()
+        revision = util.get_head_revision(branch)
+    except IOError:
+        pass
+    print >>out, '2immerse authoring tool backend, branch', branch, 'revision', revision
+    
+print_version()
