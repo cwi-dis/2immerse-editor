2f24e609ccc830c26a3890371f5b3f101d78db4c
Thomas Röggla <tom@lichtfragmente.com>
2017-08-29 17:02:48 +0200

    added tests for getTreeHeight util function

--- app/static/__tests__/utils.test.ts
+++ app/static/__tests__/utils.test.ts
@@ -2,6 +2,7 @@
 
 import { List } from "immutable";
 import * as util from "../js/editor/util";
+import { Chapter } from "../js/editor/reducers/chapters";
 
 describe("Utility function findById()", () => {
   it("finds an element in a list given by ID", () => {
@@ -27,4 +28,40 @@ describe("Utility function getTreeHeight()", () => {
   it("returns 0 on an empty list", () => {
     expect(util.getTreeHeight(List())).toBe(0);
   });
+
+  it("returns 1 on a tree with a single node", () => {
+    const tree = List(
+      [new Chapter({id: "chapter0"})]
+    );
+
+    expect(util.getTreeHeight(tree)).toBe(1);
+  });
+
+  it("returns 2 on a tree with two nodes", () => {
+    const tree = List([
+      new Chapter({id: "chapter0", children: List([
+        new Chapter({id: "chapter1"})
+      ])})
+    ]);
+
+    expect(util.getTreeHeight(tree)).toBe(2);
+  });
+
+  it("returns the length of the longest branch", () => {
+    const tree = List([
+      new Chapter({id: "chapter1", children: List([
+        new Chapter({id: "chapter1.1", children: List([
+          new Chapter({id: "chapter1.1.1"})
+        ])}),
+        new Chapter({id: "chapter1.2", children: List([
+          new Chapter({id: "chapter1.2.1", children: List([
+            new Chapter({id: "chapter1.2.1.1"})
+          ])})
+        ])})
+      ])})
+    ]);
+
+    expect(util.getTreeHeight(tree)).toBe(4);
+  });
+});
 });
