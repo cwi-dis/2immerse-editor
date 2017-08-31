e3ec31e31ac7c593700dfb283834d421854a149d
Thomas Röggla <tom@lichtfragmente.com>
2017-08-31 15:54:03 +0200

    added tests for new Screen Record class

--- app/static/__tests__/reducers/screens.test.ts
+++ app/static/__tests__/reducers/screens.test.ts
@@ -1,7 +1,44 @@
 /// <reference types="jest" />
 
 import { List } from "immutable";
-import reducer, { Screen, ScreenState } from "../../js/editor/reducers/screens";
+import reducer, { Screen, ScreenRegion, ScreenState } from "../../js/editor/reducers/screens";
+
+describe("Screen class", () => {
+  it("should instantiate a new object with the default attributes", () => {
+    const screen = new Screen();
+
+    expect(screen.id).toEqual("");
+    expect(screen.name).toEqual("");
+    expect(screen.type).toEqual("communal");
+    expect(screen.orientation).toEqual("landscape");
+    expect(screen.regions).toEqual(List());
+  });
+
+  it("should instantiate a new object with all given attributes", () => {
+    const region: ScreenRegion = {
+      id: "rootregion",
+      position: [0, 0],
+      size: [1, 1],
+      splitFrom: [null]
+    };
+
+    const screen: Screen = new Screen({
+      id: "screen1",
+      name: "Screen 1",
+      type: "personal",
+      orientation: "portrait",
+      regions: List([region]),
+    });
+
+    expect(screen.id).toEqual("screen1");
+    expect(screen.name).toEqual("Screen 1");
+    expect(screen.type).toEqual("personal");
+    expect(screen.orientation).toEqual("portrait");
+
+    expect(screen.regions.count()).toEqual(1);
+    expect(screen.regions.get(0).id).toEqual("rootregion");
+  });
+});
 
 describe("Screens reducer", () => {
   it("should return the initial state on an unknown action", () => {
