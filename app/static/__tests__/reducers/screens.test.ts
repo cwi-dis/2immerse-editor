13850b45e2e3bdbbdec9db35610e42d2783ba693
Thomas Röggla <tom@lichtfragmente.com>
2017-08-31 15:28:50 +0200

    added first tests for the screens reducer

---
+++ app/static/__tests__/reducers/screens.test.ts
@@ -0,0 +1,25 @@
+/// <reference types="jest" />
+
+import { List } from "immutable";
+import reducer, { Screen, ScreenState } from "../../js/editor/reducers/screens";
+
+describe("Screens reducer", () => {
+  it("should return the initial state on an unknown action", () => {
+    const initialState = List();
+
+    expect(
+      reducer(undefined, { type: "" })
+    ).toEqual(initialState);
+  });
+
+  it("should return the given state on an unknown action", () => {
+    const state: ScreenState = List([
+      { id: "screen1", name: "Screen 1", type: "personal", orientation: "portrait", regions: List()} as Screen,
+      { id: "screen2", name: "Screen 2", type: "communal", orientation: "landscape", regions: List()} as Screen
+    ]);
+
+    expect(
+      reducer(state, { type: "" })
+    ).toEqual(state);
+  });
+});
