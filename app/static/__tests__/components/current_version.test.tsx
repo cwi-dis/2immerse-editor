43be47f1c947f394000a5bf7c76768d2394783c9
Thomas Röggla <tom@lichtfragmente.com>
2017-09-04 19:04:31 +0200

    added test cases for CurrentVersion.state.fetchError

--- app/static/__tests__/components/current_version.test.tsx
+++ app/static/__tests__/components/current_version.test.tsx
@@ -1,7 +1,7 @@
 /// <reference types="jest" />
 
 import * as React from "react";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import { stub } from "sinon";
 
 import * as util from "../../js/editor/util";
@@ -13,11 +13,12 @@ describe("Component <CurrentVersion/>", () => {
     const stubbedFn = stub(util, "makeRequest").callsFake(() => promise);
 
     const currentVersion = mount<CurrentVersionProps, CurrentVersionState>(<CurrentVersion />);
-    expect.assertions(3);
+    expect.assertions(4);
 
     return promise.then(() => {
       expect(currentVersion.props().commitUrl).toEqual("https://gitlab-ext.irt.de/2-immerse/2immerse-editor/commit/");
       expect(currentVersion.state().hash).toEqual("some_commit_hash");
+      expect(currentVersion.state().fetchError).toBeFalsy();
 
       expect(
         currentVersion.find("a").first().props().href
@@ -34,31 +35,35 @@ describe("Component <CurrentVersion/>", () => {
     const stubbedFn = stub(util, "makeRequest").callsFake(() => promise);
 
     const currentVersion = mount<CurrentVersionProps, CurrentVersionState>(<CurrentVersion />);
-    expect.assertions(1);
+    expect.assertions(3);
 
     return promise.catch(() => {
-      expect(currentVersion.isEmptyRender()).toBeTruthy();
       stubbedFn.restore();
+    }).then(() => {
+      expect(currentVersion.isEmptyRender()).toBeTruthy();
+      expect(currentVersion.state("hash")).toEqual("");
+      expect(currentVersion.state().fetchError).toBeTruthy();
     });
   });
 
   it("should take the commit URL as a prop", () => {
-    const promise = Promise.resolve("some_commit_hash");
+    const promise = Promise.resolve("some_other_commit_hash");
     const stubbedFn = stub(util, "makeRequest").callsFake(() => promise);
 
     const currentVersion = mount<CurrentVersionProps, CurrentVersionState>(
       <CurrentVersion commitUrl="http://my-commit-url.com/" />
     );
-    expect.assertions(3);
+    expect.assertions(4);
 
     return promise.then(() => {
       expect(currentVersion.props().commitUrl).toEqual("http://my-commit-url.com/");
-      expect(currentVersion.state().hash).toEqual("some_commit_hash");
+      expect(currentVersion.state().hash).toEqual("some_other_commit_hash");
+      expect(currentVersion.state().fetchError).toBeFalsy();
 
       expect(
         currentVersion.find("a").first().props().href
       ).toEqual(
-        "http://my-commit-url.com/some_commit_hash"
+        "http://my-commit-url.com/some_other_commit_hash"
       );
 
       stubbedFn.restore();
