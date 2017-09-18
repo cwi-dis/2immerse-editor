b08041eae436f7cd2b55f11a3a6c719e1eeecb56
Thomas Röggla <tom@lichtfragmente.com>
2017-09-18 21:56:22 +0200

    putting component ID into dataTransfer object on drag start

--- app/static/js/editor/components/master_manager/dmappc_container.tsx
+++ app/static/js/editor/components/master_manager/dmappc_container.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 
 class DMAppcContainer extends React.Component<{}, {}> {
+  public setDragData(componentId: string, e: React.DragEvent<HTMLDivElement>) {
+    e.dataTransfer.setData("text/plain", componentId);
+  }
+
   public render() {
     return (
       <div style={{height: "50%", overflowY: "scroll", backgroundColor: "#353535", borderTop: "1px solid #161616", padding: 2}}>
         {Array(11).fill(null).map((_, i: number) => {
           return (
-            <div key={i} draggable style={{backgroundColor: "#262626", margin: 3, width: 140, height: 140, padding: 5, float: "left"}}>
+            <div key={i}
+                 draggable onDragStart={this.setDragData.bind(this, `component${i}`)}
+                 style={{backgroundColor: "#262626", margin: 3, width: 140, height: 140, padding: 5, float: "left"}}>
               <b style={{fontSize: 15}}>DMAppC {i}</b>
               <p style={{marginTop: 10, fontSize: 12}}>This is a short description</p>
             </div>
