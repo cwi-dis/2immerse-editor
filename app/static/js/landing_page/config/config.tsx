fc21bb93143bd0a6c9d44f04aec0914f131b62e2
Thomas Röggla <tom@lichtfragmente.com>
2017-09-20 18:26:42 +0200

    styled config form

--- app/static/js/landing_page/config/config.tsx
+++ app/static/js/landing_page/config/config.tsx
@@ -1,42 +1,69 @@
 import * as React from "react";
 
-import { URLInputField, CheckboxInputField, SelectInputField } from "./input_fields";
+import { URLInputField, CheckboxInputField, SelectInputField, FileInputField } from "./input_fields";
 import CurrentVersion from "../../editor/components/current_version";
 
 class Config extends React.Component<{}, {}> {
   public render() {
+    const boxStyle: React.CSSProperties = {
+      width: "70vw",
+      margin: "40px auto 40px auto",
+      backgroundColor: "#EFEFEF",
+      padding: 25,
+      borderRadius: 15,
+    };
+
     return (
-      <div className="container">
-        <div className="content">
-          <h3>Configuration</h3>
-          <br/>
+      <div style={boxStyle}>
+        <h3 style={{fontSize: 25, color: "#555555"}}>Configuration</h3>
+        <br/>
 
-          <URLInputField label="Client API" />
-          <URLInputField label="Layout Service" />
-          <URLInputField label="Timeline Service" />
-          <URLInputField label="Websocket Service" />
-          <CheckboxInputField label="Kibana" description="Enable Kibana" defaultValue={false} />
-          <SelectInputField label="Mode" options={["standalone"]} />
-          <SelectInputField label="Log Level" options={["DEBUG"]} />
-          <br/>
+        <h4>Upload JSON Config File</h4>
+        <br/>
 
-          <div className="field is-horizontal">
-            <div className="field-label"></div>
-            <div className="field-body">
-              <div className="field">
-                <div className="control">
-                  <button className="button is-info">
-                    Save Config
-                  </button>
-                </div>
+        <FileInputField label="Config File" />
+        <br/>
+
+        <div className="field is-horizontal">
+          <div className="field-label"></div>
+          <div className="field-body">
+            <div className="field">
+              <div className="control">
+                <button className="button is-info">
+                  Upload Config
+                </button>
               </div>
             </div>
           </div>
+        </div>
+        <br/>
 
+        <h4>Manual Configuration</h4>
+        <br/>
+        <URLInputField label="Client API" />
+        <URLInputField label="Layout Service" />
+        <URLInputField label="Timeline Service" />
+        <URLInputField label="Websocket Service" />
+        <CheckboxInputField label="Kibana" description="Enable Kibana" defaultValue={false} />
+        <SelectInputField label="Mode" options={["standalone"]} />
+        <SelectInputField label="Log Level" options={["DEBUG"]} />
+        <br/>
+
+        <div className="field is-horizontal">
+          <div className="field-label"></div>
+          <div className="field-body">
+            <div className="field">
+              <div className="control">
+                <button className="button is-info">
+                  Save Config
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <CurrentVersion />
-      </div>
+      <CurrentVersion />
+    </div>
     );
   }
 }
