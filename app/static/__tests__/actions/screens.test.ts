1824126b3241e3bc2720b4495741bfc182ae2a69
Thomas Röggla <tom@lichtfragmente.com>
2017-09-20 14:04:43 +0200

    added tests for async screen actions

--- app/static/__tests__/actions/screens.test.ts
+++ app/static/__tests__/actions/screens.test.ts
@@ -1,7 +1,12 @@
 /// <reference types="jest" />
 
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { List } from "immutable";
+
 import * as actionTypes from "../../js/editor/actions/screens";
 import { actionCreators } from "../../js/editor/actions/screens";
+import { Screen as ScreenModel, ScreenRegion } from "../../js/editor/reducers/screens";
 
 describe("Screen actions", () => {
   it("should create an ADD_DEVICE action for a communal screen", () => {
@@ -85,5 +90,105 @@ describe("Screen actions", () => {
 
     expect(actionCreators.updateSelectedScreen()).toEqual(expected);
     expect(actionCreators.updateSelectedScreen(undefined)).toEqual(expected);
+  });
+});
+
+describe("Async screen actions", () => {
+  const mockStore = configureMockStore([thunk]);
+
+  it("should create REMOVE_DEVICE and REMOVE_SCREEN_FROM_LAYOUTS actions when calling removeDeviceAndUpdateMasters()", () => {
+    const expectedActions = [
+      { type: "REMOVE_DEVICE", payload: { id: "screen1" }},
+      { type: "REMOVE_SCREEN_FROM_LAYOUTS", payload: { screenId: "screen1" }}
+    ];
+
+    const store = mockStore({ screens: { currentScreen: undefined } });
+    store.dispatch(actionCreators.removeDeviceAndUpdateMasters("screen1"));
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it("should create create an UPDATE_SELECTED_SCREEN action if screenId == currentScreen when calling removeDeviceAndUpdateMasters()", () => {
+    const expectedActions = [
+      { type: "UPDATE_SELECTED_SCREEN", payload: { screenId: undefined }},
+      { type: "REMOVE_DEVICE", payload: { id: "screen1" }},
+      { type: "REMOVE_SCREEN_FROM_LAYOUTS", payload: { screenId: "screen1" }}
+    ];
+
+    const store = mockStore({ screens: { currentScreen: "screen1" } });
+    store.dispatch(actionCreators.removeDeviceAndUpdateMasters("screen1"));
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it("should create UNDO_LAST_SPLIT and REMOVE_REGION_FROM_LAYOUTS actions when calling undoLastSplitAndUpdateMasters()", () => {
+    const expectedActions = [
+      { type: "UNDO_LAST_SPLIT", payload: { screenId: "screen2" }},
+      { type: "REMOVE_REGION_FROM_LAYOUTS", payload: { regionId: "lastRegion" }}
+    ];
+
+    const store = mockStore({
+      screens: {
+        currentScreen: undefined,
+        previewScreens: List([
+          new ScreenModel({id: "screen1", name: "Screen 1", regions: List(), type: "communal", orientation: "landscape"}),
+          new ScreenModel({id: "screen2", name: "Screen 1", regions: List<ScreenRegion>([
+            {id: "region1", position: [0, 0], size: [0.5, 1], splitFrom: [null]},
+            {id: "lastRegion", position: [0.5, 0], size: [0.5, 1], splitFrom: [null, "region1"]}
+          ]), type: "communal", orientation: "landscape"}),
+        ])
+      }
+    });
+    store.dispatch(actionCreators.undoLastSplitAndUpdateMasters("screen2"));
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it("should not create any actions when calling undoLastSplitAndUpdateMasters() on a screen with a single region", () => {
+    const store = mockStore({
+      screens: {
+        currentScreen: undefined,
+        previewScreens: List([
+          new ScreenModel({id: "screen1", name: "Screen 1", regions: List(), type: "communal", orientation: "landscape"}),
+          new ScreenModel({id: "screen2", name: "Screen 1", regions: List<ScreenRegion>([
+            {id: "lastRegion", position: [0.5, 0], size: [0.5, 1], splitFrom: [null, "region1"]}
+          ]), type: "communal", orientation: "landscape"}),
+        ])
+      }
+    });
+    store.dispatch(actionCreators.undoLastSplitAndUpdateMasters("screen2"));
+
+    expect(store.getActions().length).toEqual(0);
+  });
+
+  it("should not create any actions when calling undoLastSplitAndUpdateMasters() on a screen with no regions", () => {
+    const store = mockStore({
+      screens: {
+        currentScreen: undefined,
+        previewScreens: List([
+          new ScreenModel({id: "screen1", name: "Screen 1", regions: List(), type: "communal", orientation: "landscape"}),
+        ])
+      }
+    });
+    store.dispatch(actionCreators.undoLastSplitAndUpdateMasters("screen1"));
+
+    expect(store.getActions().length).toEqual(0);
+  });
+
+  it("should not create any actions when calling undoLastSplitAndUpdateMasters() on a screen that cannot be found", () => {
+    const store = mockStore({
+      screens: {
+        currentScreen: undefined,
+        previewScreens: List([
+          new ScreenModel({id: "screen1", name: "Screen 1", regions: List(), type: "communal", orientation: "landscape"}),
+          new ScreenModel({id: "screen2", name: "Screen 1", regions: List<ScreenRegion>([
+            {id: "lastRegion", position: [0.5, 0], size: [0.5, 1], splitFrom: [null, "region1"]}
+          ]), type: "communal", orientation: "landscape"}),
+        ])
+      }
+    });
+    store.dispatch(actionCreators.undoLastSplitAndUpdateMasters("screen3"));
+
+    expect(store.getActions().length).toEqual(0);
   });
 });
