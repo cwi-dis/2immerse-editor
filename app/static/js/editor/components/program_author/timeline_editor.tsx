857ed7272527bb0aae400032638f92bbb5506ebd
Thomas Röggla <tom@lichtfragmente.com>
2017-10-10 15:56:02 +0200

    redesigned state for TimelineEditor to accommodate multiple tracks

--- app/static/js/editor/components/program_author/timeline_editor.tsx
+++ app/static/js/editor/components/program_author/timeline_editor.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
-import { List } from "immutable";
+import { List, Record } from "immutable";
 
 import { findById } from "../../util";
 import Timeline, { TimelineElement } from "./timeline_track";
+
+class TimelineTrack extends Record<{timelineElements: List<TimelineElement>}>({ timelineElements: List() }) {
+}
 
 interface TimelineEditorProps {
 }
 
 interface TimelineEditorState {
-  timelineElements: List<TimelineElement>;
+  timelines: List<TimelineTrack>;
 }
 
 class TimelineEditor extends React.Component<{}, TimelineEditorState> {
@@ -17,20 +20,33 @@ class TimelineEditor extends React.Component<{}, TimelineEditorState> {
     super(props);
 
     this.state = {
-      timelineElements: List([
-        { id: "one", x: 100, width: 200},
-        { id: "two", x: 320, width: 300},
-        { id: "three", x: 800, width: 150},
+      timelines: List([
+        new TimelineTrack({
+          timelineElements: List([
+            { id: "one", x: 100, width: 200},
+            { id: "two", x: 320, width: 300},
+            { id: "three", x: 800, width: 150}
+          ])
+        }),
+        new TimelineTrack({
+          timelineElements: List([
+            { id: "four", x: 10, width: 400},
+            { id: "five", x: 450, width: 100},
+            { id: "six", x: 600, width: 200}
+          ])
+        })
       ])
     };
   }
 
-  private elementPositionUpdated(id: string, x: number) {
-    const [i, element] = findById(this.state.timelineElements, id);
+  private elementPositionUpdated(index: number, id: string, x: number) {
+    const [i, element] = findById(this.state.timelines.get(index)!.timelineElements, id);
     console.log("updating", element, "with new x", x);
 
     this.setState({
-      timelineElements: this.state.timelineElements.set(i, {...element, x})
+      timelines: this.state.timelines.updateIn([index, "timelineElements", i], (element) => {
+        return {...element, x};
+      })
     });
   }
 
@@ -39,9 +55,12 @@ class TimelineEditor extends React.Component<{}, TimelineEditorState> {
       <div className="columnlayout">
         <div className="column-content" style={{flexGrow: 1}}>
           <h3>Timeline Editor</h3>
-          <Timeline elements={this.state.timelineElements}
-                    elementPositionUpdated={this.elementPositionUpdated.bind(this)}
+          <Timeline elements={this.state.timelines.getIn([0, "timelineElements"])}
+                    elementPositionUpdated={this.elementPositionUpdated.bind(this, 0)}
                     width={1000} height={40} />
+          <Timeline elements={this.state.timelines.getIn([1, "timelineElements"])}
+                    elementPositionUpdated={this.elementPositionUpdated.bind(this, 1)}
+                    width={1000} height={40} snapDistance={15} />
         </div>
         <div className="column-sidebar">
           sidebar
