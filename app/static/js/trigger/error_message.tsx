5c3b7c8a7482fe8cfc96f446395a6c8daf8e3ddc
Thomas Röggla <tom@lichtfragmente.com>
2017-10-17 14:52:46 +0200

    made document ID optional in props of ErrorMessage component

--- app/static/js/trigger/error_message.tsx
+++ app/static/js/trigger/error_message.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 interface ErrorMessageProps {
-  documentId: string;
+  documentId?: string;
   statusText: string;
   status: number;
 }
@@ -14,14 +14,16 @@ const ErrorMessage: React.SFC<ErrorMessageProps> = (props) => {
           <p><strong>ERROR</strong>!</p>
         </div>
         <div className="message-body" style={{backgroundColor: "#555555", color: "#FFFFFF"}}>
-          An error occurred while trying to fetch events for the document with
-          the ID <i>{props.documentId}</i>:
+          {(props.documentId)
+            ? <span>An error occurred while trying to fetch events for the document with the ID <i>{props.documentId}</i>:</span>
+            : <span>An error occurred while trying to complete your request:</span>
+          }
 
           <div style={{margin: 25, fontWeight: "bold", textAlign: "center"}}>
             {props.statusText} (HTTP error {props.status})
           </div>
 
-          Try to clear the session and start over.
+          {(props.documentId) && <span>Try to clear the session and start over.</span>}
         </div>
       </article>
     </div>
