d945a0689dbe75c1e048e147f5eae25f7453e61b
Thomas Röggla <tom@lichtfragmente.com>
2017-10-18 17:46:07 +0200

    fetching preview player status every second

--- app/static/js/trigger/remote_control.tsx
+++ app/static/js/trigger/remote_control.tsx
@@ -1,11 +1,58 @@
 import * as React from "react";
+import { makeRequest } from "../editor/util";
 
 interface RemoteControlProps {
   documentId: string;
 }
 
-class RemoteControl extends React.Component<RemoteControlProps, {}> {
+interface PreviewStatus {
+  active: boolean;
+  status: string;
+  playing?: boolean;
+  position?: number;
+}
+
+interface RemoteControlState {
+  previewStatus: PreviewStatus;
+}
+
+class RemoteControl extends React.Component<RemoteControlProps, RemoteControlState> {
+  private statusInterval: any;
+
+  public constructor(props: RemoteControlProps) {
+    super(props);
+
+    this.state = {
+      previewStatus: {
+        active: false,
+        status: "Preview player is not running"
+      }
+    };
+  }
+
+  public componentDidMount() {
+    this.statusInterval = setInterval(() => {
+      makeRequest("GET", `/api/v1/document/${this.props.documentId}/remote`).then((data) => {
+        const previewStatus = JSON.parse(data);
+
+        this.setState({
+          previewStatus
+        });
+      }).catch((err) => {
+        console.error("Could not fetch preview status:", err);
+      });
+    }, 1000);
+  }
+
+  public componentWillUnmount() {
+    if (this.statusInterval) {
+      clearInterval(this.statusInterval);
+    }
+  }
+
   public render() {
+    const { previewStatus } = this.state;
+
     const style: React.CSSProperties = {
       position: "fixed",
       height: 60,
