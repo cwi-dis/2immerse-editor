efae31ff77e89770e64cc9417f3c7a41b50b45e9
Thomas Röggla <tom@lichtfragmente.com>
2017-11-15 16:27:56 +0100

    added file and config for test setup with polyfill

---
+++ app/static/__tests__/setup.ts
@@ -0,0 +1,8 @@
+global.requestAnimationFrame = function (callback) {
+  setTimeout(callback, 0);
+};
+
+import { configure } from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
