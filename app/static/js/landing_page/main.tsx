8726bdca7872c218b2a01dd3f25c02332dd85233
Thomas Röggla <tom@lichtfragmente.com>
2017-11-20 16:40:16 +0100

    updated landing page to new react-router API

--- app/static/js/landing_page/main.tsx
+++ app/static/js/landing_page/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { Router, Route, hashHistory } from "react-router";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import LandingPage from "./landing_page";
 import Config from "./config/config";
@@ -10,10 +10,12 @@ import "../../css/style.css";
 
 window.onload = () => {
   render(
-    <Router history={hashHistory}>
-      <Route path="/" component={LandingPage} />
-      <Route path="/config" component={Config} />
-    </Router>,
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/config" component={Config} />
+      </Switch>
+    </HashRouter>,
     document.getElementById("react")
   );
 };
