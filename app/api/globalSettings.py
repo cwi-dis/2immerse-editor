8ed58c8f18f53b14a45239f5a520e5933072c902
Thomas Röggla <tom@lichtfragmente.com>
2017-11-23 14:47:39 +0100

    wrapped global config into a class and updated methods

--- app/api/globalSettings.py
+++ app/api/globalSettings.py
@@ -7,34 +7,50 @@
 # environment variables that override the URLs
 import os
 
-# URL for the preview player:
-clientApiUrl = os.getenv("CLIENT_API_URL", "https://origin.platform.2immerse.eu/client-api/master/dist/test/general-test/dist/index.html")
 
-# Mode in which the preview player runs (tv or standalone)
-mode = "standalone"
+class GlobalSettings(object):
+    # URL for the preview player:
+    clientApiUrl = os.getenv(
+        "CLIENT_API_URL",
+        "https://origin.platform.2immerse.eu/client-api/master/dist/test/general-test/dist/index.html"
+    )
 
-# URLs for 2immerse services
-layoutService = os.getenv("LAYOUT_SERVICE_URL", "https://layout-service-edge.platform.2immerse.eu/layout/v4")
-websocketService = os.getenv("WEBSOCKET_SERVICE_URL", "https://websocket-service-edge.platform.2immerse.eu/")
-timelineService = os.getenv("TIMELINE_SERVICE_URL", "https://timeline-service-edge.platform.2immerse.eu/timeline/v1")
-kibanaService = os.getenv("KIBANA_SERVICE_URL", "https://platform.2immerse.eu/kibana/app/kibana")
+    # URLs for 2immerse services
+    layoutService = os.getenv(
+        "LAYOUT_SERVICE_URL",
+        "https://layout-service-edge.platform.2immerse.eu/layout/v4"
+    )
+    websocketService = os.getenv(
+        "WEBSOCKET_SERVICE_URL",
+        "https://websocket-service-edge.platform.2immerse.eu/"
+    )
+    timelineService = os.getenv(
+        "TIMELINE_SERVICE_URL",
+        "https://timeline-service-edge.platform.2immerse.eu/timeline/v1"
+    )
+    kibanaService = os.getenv(
+        "KIBANA_SERVICE_URL",
+        "https://platform.2immerse.eu/kibana/app/kibana"
+    )
 
-# Logging parameters for the authoring service
-noKibana = (kibanaService == "")
-logLevel = 'werkzeug:WARN,INFO'
+    # Mode in which the preview player runs (tv or standalone)
+    mode = "standalone"
+
+    # Logging parameters for the authoring service
+    noKibana = (kibanaService == "")
+    logLevel = 'werkzeug:WARN,INFO'
+
 
 def _get():
-    kList = [name for name in globals() if name[:1] != '_']
-    rv = {}
-    for k in kList:
-        rv[k] = globals()[k]
-    return rv
+    props = GlobalSettings.__dict__
+    return {k: v for k, v in props.iteritems() if k[:1] != "_"}
+
 
 def _put(values):
-    for k, v in values.items():
-        globals()[k] = v
+    for k, v in values.iteritems():
+        setattr(GlobalSettings, k, v)
 
 if __name__ == '__main__':
     print _get()
-    _put({'noKibana':True})
+    _put({'noKibana': True})
     print _get()
