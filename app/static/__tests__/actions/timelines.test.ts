a4ee31074a60a6d3a5d1746cf184cb54872c0e7b
Thomas Röggla <tom@lichtfragmente.com>
2017-12-13 15:24:34 +0100

    added test cases for new timeline actions

---
+++ app/static/__tests__/actions/timelines.test.ts
@@ -0,0 +1,29 @@
+/// <reference types="jest" />
+
+import * as actionTypes from "../../js/editor/actions/timelines";
+import { actionCreators } from "../../js/editor/actions/timelines";
+
+describe("Timeline actions", () => {
+  it("should create an ADD_TIMELINE action", () => {
+    const expected: actionTypes.ADD_TIMELINE = {
+      type: "ADD_TIMELINE",
+      payload: {
+        chapterId: "chapter1"
+      }
+    };
+
+    expect(actionCreators.addTimeline("chapter1")).toEqual(expected);
+  });
+
+  it("should create an ADD_TIMELINE_TRACK action", () => {
+    const expected: actionTypes.ADD_TIMELINE_TRACK = {
+      type: "ADD_TIMELINE_TRACK",
+      payload: {
+        chapterId: "chapter1",
+        componentId: "component1"
+      }
+    };
+
+    expect(actionCreators.addTimelineTrack("chapter1", "component1")).toEqual(expected);
+  });
+});
