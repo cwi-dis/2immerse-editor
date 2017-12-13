83797f797d07c50879ff21d276e569ad3d4f7d3a
Thomas Röggla <tom@lichtfragmente.com>
2017-12-13 14:52:22 +0100

    added action for adding a timeline and a timeline track

--- app/static/js/editor/actions/timelines.ts
+++ app/static/js/editor/actions/timelines.ts
@@ -1,7 +1,33 @@
 import { ActionCreatorsMapObject } from "redux";
+import { PayloadAction } from "../util";
+
+export type ADD_TIMELINE = PayloadAction<"ADD_TIMELINE", {chapterId: string}>;
+function addTimeline(chapterId: string): ADD_TIMELINE {
+  return {
+    type: "ADD_TIMELINE",
+    payload: {
+      chapterId
+    }
+  };
+}
+
+export type ADD_TIMELINE_TRACK = PayloadAction<"ADD_TIMELINE_TRACK", {chapterId: string, componentId: string}>;
+function addTimelineTrack(chapterId: string, componentId: string): ADD_TIMELINE_TRACK {
+  return {
+    type: "ADD_TIMELINE_TRACK",
+    payload: {
+      chapterId,
+      componentId
+    }
+  };
+}
 
 export interface TimelineActions extends ActionCreatorsMapObject {
+  addTimeline: (chapterId: string) => ADD_TIMELINE;
+  addTimelineTrack: (chapterId: string, componentId: string) => ADD_TIMELINE_TRACK;
 }
 
 export const actionCreators: TimelineActions = {
+  addTimeline,
+  addTimelineTrack
 };
