8a1b9bf7fa3f46c2b870d2b120c50be9a706147b
Thomas Röggla <tom@lichtfragmente.com>
2017-12-24 15:50:03 +0100

    added reducer for REMOVE_ELEMENT_FROM_TIMELINE_TRACK track

--- app/static/js/editor/reducers/timelines.ts
+++ app/static/js/editor/reducers/timelines.ts
@@ -158,4 +158,19 @@ actionHandler.addHandler("UPDATE_ELEMENT_POSITION", (state, action: actions.UPDA
   });
 });
 
+actionHandler.addHandler("REMOVE_ELEMENT_FROM_TIMELINE_TRACK", (state, action: actions.REMOVE_ELEMENT_FROM_TIMELINE_TRACK) => {
+  const { timelineId, trackId, elementId } = action.payload;
+  const [timelinenum] = findById(state, timelineId);
+
+  return state.updateIn([timelinenum, "timelineTracks"], (tracks: List<TimelineTrack>) => {
+    const [tracknum] = findById(tracks, trackId);
+
+    return tracks.updateIn([tracknum, "timelineElements"], (elements: List<TimelineElement>) => {
+      const [elementnum] = findById(elements, elementId);
+
+      return elements.remove(elementnum);
+    });
+  });
+});
+
 export default actionHandler.getReducer();
