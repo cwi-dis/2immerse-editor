5fdf545c71e10f5447ba34ed3af0cf4aebd7a099
Thomas Röggla <tom@lichtfragmente.com>
2018-04-04 02:06:01 +0200

    moved param functionality from EventContainer to EventModal

--- app/static/js/trigger/event_container.tsx
+++ app/static/js/trigger/event_container.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
 import * as classNames from "classnames";
-import { List } from "immutable";
 
-import { capitalize, makeRequest } from "../editor/util";
-import { Event, EventParams } from "./trigger_client";
-import ParamInputField from "./param_input_field";
+import { Event } from "./trigger_client";
 import EventModal from "./utils/event_modal";
 
 interface EventContainerProps {
@@ -17,16 +14,8 @@ interface EventContainerState {
   isLoading: boolean;
   flashSuccess: boolean;
   flashError: boolean;
-  params: List<EventParams>;
   showEventModal: boolean;
 }
-
-const paramDefaults: {[key: string]: string} = {
-  duration: "0",
-  time: "0",
-  string: "",
-  url: ""
-};
 
 class EventContainer extends React.Component<EventContainerProps, EventContainerState> {
   constructor(props: EventContainerProps) {
@@ -36,84 +25,18 @@ class EventContainer extends React.Component<EventContainerProps, EventContainer
       isLoading: false,
       flashSuccess: false,
       flashError: false,
-      params: this.convertParams(props.event.parameters),
       showEventModal: false
     };
   }
 
-  private convertParams(parameters: Array<EventParams>) {
-    return List(parameters.map((param) => {
-      param.value = param.value ? param.value : paramDefaults[param.type];
-
-      if (param.type === "selection" && param.options) {
-        param.value = param.options[0].value;
-      }
-
-      return param;
-    }));
-  }
-
-  private collectParams(): List<{parameter: string, value: string}> {
-    return this.state.params.map((param) => {
-      return {
-        parameter: param.parameter,
-        value: param.value!
-      };
-    });
-  }
-
-  private launchEvent() {
-    const { event, documentId } = this.props;
-
-    const endpoint = event.modify ? "modify" : "trigger";
-    const requestMethod = event.modify ? "PUT" : "POST";
-
-    const url = `/api/v1/document/${documentId}/events/${event.id}/${endpoint}`;
-    const data = JSON.stringify(this.collectParams());
-
-    console.log("Launching event at url", url, "with data", data);
-    this.setState({
-      isLoading: true
-    });
-
-    makeRequest(requestMethod, url, data, "application/json").then((data) => {
-      console.log("success");
-
-      this.setState({
-        isLoading: false,
-        flashSuccess: true
-      });
-
-      this.props.onTriggered && this.props.onTriggered();
-    }).catch((err) => {
-      console.log("error:", err);
-
-      this.setState({
-        isLoading: false,
-        flashError: true
-      });
-    });
-  }
-
-  private updateParamField(i: number, ev: React.ChangeEvent<HTMLInputElement>) {
-    let currentValue = this.state.params.get(i)!;
-    currentValue.value = ev.target.value;
-
-    this.setState({
-      params: this.state.params.set(i, currentValue)
-    });
-  }
-
   private getButtonLabel(): string {
-    const { event } = this.props;
+    const { parameters } = this.props.event;
 
-    if (event.verb) {
-      return event.verb;
-    } else if (event.modify) {
-      return "Modify";
+    if (parameters.filter((param) => param.type !== "set").length > 0) {
+      return "configure";
     }
 
-    return "Trigger";
+    return "show";
   }
 
   private renderEventModal() {
@@ -125,7 +48,15 @@ class EventContainer extends React.Component<EventContainerProps, EventContainer
       <div className="modal is-active">
         <div className="modal-background"></div>
         <div className="modal-content">
-          <EventModal event={this.props.event} />
+          <EventModal event={this.props.event}
+                      documentId={this.props.documentId}
+                      onTriggered={(status) => {
+                        this.setState({
+                          showEventModal: false,
+                          flashSuccess: status === "success",
+                          flashError: status === "error"
+                        });
+                      }} />
         </div>
         <button className="modal-close is-large"
                 onClick={() => this.setState({showEventModal: false})}>
@@ -134,45 +65,25 @@ class EventContainer extends React.Component<EventContainerProps, EventContainer
     );
   }
 
-  private renderParamTable() {
-    const { params } = this.state;
+  private paramCount() {
+    const { event } = this.props;
+    const count = event.parameters.filter((param) => param.type !== "set").length;
 
-    if (params.count() > 0) {
-      return (
-        <table className="table is-narrow" style={{width: "80%", margin: "20px 0 15px 0"}}>
-          <tbody>
-            {params.map((param, i) => {
-              if (param.type === "set") {
-                return;
-              }
-
-              return (
-                <tr key={i}>
-                  <td style={{width: "50%", verticalAlign: "middle", border: "none"}}>
-                    {capitalize(param.name)}
-                  </td>
-                  <td style={{width: "50%", border: "none"}}>
-                    <ParamInputField {...param}
-                                     onChange={this.updateParamField.bind(this, i)} />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      );
+    if (count === 0) {
+      return null;
     }
+
+    return (
+      <p>{count} parameter{(count === 1) ? "" : "s"}</p>
+    );
   }
 
   public render() {
     const { event } = this.props;
-    const { params, isLoading, flashSuccess, flashError } = this.state;
-
-    const submitEnabled = params.filter((p) => p.required)
-                                .every((p) => p.value !== undefined && p.value !== "");
+    const { isLoading, flashSuccess, flashError } = this.state;
 
     return (
-      <div style={{width: 400, border: "1px solid #161616", borderRadius: 5, boxShadow: "0 0 10px #161616", margin: "10px 25px 5px 25px", padding: 25}}>
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "space-between", width: 400, border: "1px solid #161616", borderRadius: 5, boxShadow: "0 0 10px #161616", margin: 10, padding: 25}}>
         <div style={{display: "flex"}}>
           <div style={{width: 100, height: 100, margin: "0 15px 0 0"}}>
             {(event.previewUrl) && <img src={event.previewUrl} style={{maxWidth: 98, maxHeight: 98}} />}
@@ -180,16 +91,16 @@ class EventContainer extends React.Component<EventContainerProps, EventContainer
           <div>
             <h3 style={{color: "#E9E9E9"}}>{event.name}</h3>
             {(event.longdesc) && <p>{event.longdesc}</p>}
-            {this.renderParamTable()}
+            {this.paramCount()}
           </div>
         </div>
 
-        <div>
+        <div style={{ marginTop: 20 }}>
           <button className={classNames(
                               "button",
                               "is-info",
                               {"is-loading": isLoading, "button-pulse-success": flashSuccess, "button-pulse-error": flashError})}
-                  onClick={() => (params.count() === 0) ? this.launchEvent.bind(this) : this.setState({showEventModal: true})}
+                  onClick={() => this.setState({showEventModal: true})}
                   onAnimationEnd={() => this.setState({flashSuccess: false, flashError: false})}>
             {this.getButtonLabel()}
           </button>
--- app/static/js/trigger/utils/event_modal.tsx
+++ app/static/js/trigger/utils/event_modal.tsx
@@ -1,14 +1,147 @@
 import * as React from "react";
-import { Event } from "../trigger_client";
+import { List } from "immutable";
+
+import { capitalize, makeRequest } from "../../editor/util";
+import { Event, EventParams } from "../trigger_client";
+import ParamInputField from "../param_input_field";
 
 interface EventModalProps {
   event: Event;
+  documentId: string;
+
+  onTriggered: (status: "success" | "error") => void;
 }
 
-class EventModal extends React.Component<EventModalProps, {}> {
+interface EventModalState {
+  params: List<EventParams>;
+}
+
+const paramDefaults: {[key: string]: string} = {
+  duration: "0",
+  time: "0",
+  string: "",
+  url: ""
+};
+
+class EventModal extends React.Component<EventModalProps, EventModalState> {
+  public constructor(props: EventModalProps) {
+    super(props);
+
+    this.state = {
+      params: this.convertParams(props.event.parameters)
+    };
+  }
+
+  private convertParams(parameters: Array<EventParams>) {
+    return List(parameters.map((param) => {
+      param.value = param.value ? param.value : paramDefaults[param.type];
+
+      if (param.type === "selection" && param.options) {
+        param.value = param.options[0].value;
+      }
+
+      return param;
+    }));
+  }
+
+  private collectParams(): List<{parameter: string, value: string}> {
+    return this.state.params.map((param) => {
+      return {
+        parameter: param.parameter,
+        value: param.value!
+      };
+    });
+  }
+
+  private launchEvent() {
+    const { event, documentId, onTriggered } = this.props;
+
+    const endpoint = event.modify ? "modify" : "trigger";
+    const requestMethod = event.modify ? "PUT" : "POST";
+
+    const url = `/api/v1/document/${documentId}/events/${event.id}/${endpoint}`;
+    const data = JSON.stringify(this.collectParams());
+
+    console.log("Launching event at url", url, "with data", data);
+
+    makeRequest(requestMethod, url, data, "application/json").then((data) => {
+      console.log("success");
+      onTriggered("success");
+    }).catch((err) => {
+      console.log("error:", err);
+      onTriggered("error");
+    });
+  }
+
+  private updateParamField(i: number, ev: React.ChangeEvent<HTMLInputElement>) {
+    let currentValue = this.state.params.get(i)!;
+    currentValue.value = ev.target.value;
+
+    this.setState({
+      params: this.state.params.set(i, currentValue)
+    });
+  }
+
+  private renderParamTable() {
+    const { params } = this.state;
+
+    if (params.count() > 0) {
+      return (
+        <table className="table is-narrow" style={{margin: "20px 0 15px 0"}}>
+          <tbody>
+            {params.map((param, i) => {
+              if (param.type === "set") {
+                return;
+              }
+
+              return (
+                <tr key={i}>
+                  <td style={{width: "20%", verticalAlign: "middle", border: "none", color: "#000000"}}>
+                    {capitalize(param.name)}
+                  </td>
+                  <td style={{width: "80%", border: "none"}}>
+                    <ParamInputField {...param}
+                                     onChange={this.updateParamField.bind(this, i)} />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      );
+    }
+  }
+
+  private getButtonLabel(): string {
+    const { event } = this.props;
+
+    if (event.verb) {
+      return event.verb;
+    } else if (event.modify) {
+      return "Modify";
+    }
+
+    return "Trigger";
+  }
+
   public render() {
+    const { params } = this.state;
+    const submitEnabled = params.filter((p) => p.required)
+                                .every((p) => p.value !== undefined && p.value !== "");
+
+    if (params.filter((param) => param.type !== "set").count() === 0) {
+      this.launchEvent();
+      return null;
+    }
+
     return (
-      <div className="box">Hello World</div>
+      <div className="box">
+        {this.renderParamTable()}
+        <br/>
+        <button className="button is-info"
+                onClick={this.launchEvent.bind(this)}
+                disabled={!submitEnabled}>{this.getButtonLabel()}</button>
+      </div>
     );
   }
 }
