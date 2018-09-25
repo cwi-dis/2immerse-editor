b4c34f81276b4eebb18fc56092252730f7f50575
Jack Jansen <Jack.Jansen@cwi.nl>
2018-09-25 11:42:33 +0200

    Added testcase for clocks

---
+++ test/test_clocks.py
@@ -0,0 +1,116 @@
+import unittest
+import time
+
+import pretest
+import clocks
+
+
+class TestClocks(unittest.TestCase):
+    DELTA_T = 0.01
+
+    def test_systemClock(self):
+        """Test that SystemClock runs at the right speed"""
+        clock = clocks.SystemClock()
+        start = clock.now()
+        self.assertAlmostEqual(start, time.time(), delta=self.DELTA_T)
+        
+        now = clock.now()
+        self.assertAlmostEqual(now, start, delta=self.DELTA_T)
+        
+        clock.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+1, delta=self.DELTA_T)
+        
+        time.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+2, delta=self.DELTA_T)
+        
+    def test_fastClock(self):
+        """Test that FastClock runs at the right speed"""
+        clock = clocks.FastClock()
+        start = clock.now()
+        self.assertAlmostEqual(start, 0, delta=self.DELTA_T)
+        now = clock.now()
+        self.assertAlmostEqual(now, start, delta=self.DELTA_T)
+        
+        clock.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+1, delta=self.DELTA_T)
+        
+        time.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+1, delta=self.DELTA_T)
+        
+    def test_pauseableClock(self):
+        """Test a PausableCLock with underlying FastClock"""
+        underlyingClock = clocks.FastClock()
+        clock = clocks.PausableClock(underlyingClock)
+        
+        self.assertEqual(clock.getRate(), 0.0)
+        clock.start()
+        self.assertEqual(clock.getRate(), 1.0)
+        clock.stop()
+        self.assertEqual(clock.getRate(), 0.0)
+        clock.start()
+        
+        start = clock.now()
+        self.assertAlmostEqual(start, 0, delta=self.DELTA_T)
+        now = clock.now()
+        self.assertAlmostEqual(now, start, delta=self.DELTA_T)
+        
+        underlyingClock.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+1, delta=self.DELTA_T)
+        
+        clock.stop()
+        underlyingClock.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+1, delta=self.DELTA_T)
+        
+        clock.start()
+        underlyingClock.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+2, delta=self.DELTA_T)
+        
+        time.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+2, delta=self.DELTA_T)
+        
+        clock.set(start+10)
+        underlyingClock.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+11, delta=self.DELTA_T)
+        
+        clock.stop()
+        clock.set(start+20)
+        underlyingClock.sleep(1)
+        now = clock.now()
+        self.assertAlmostEqual(now, start+20, delta=self.DELTA_T)
+        
+    def callbackHelper(self, arg):
+        self.callbackArg = arg
+        
+    def schedule(self, callback, *args, **kwargs):
+        callback(*args, **kwargs)
+        
+    def test_callbackPausableClock(self):
+        self.callbackArg = None
+        underlyingClock = clocks.FastClock()
+        clock = clocks.CallbackPausableClock(underlyingClock)
+        clock.start()
+        start = clock.now()
+        self.assertAlmostEqual(start, 0, delta=self.DELTA_T)
+        
+        self.assertEqual(clock.nextEventTime(), clocks.never)
+        
+        clock.schedule(10, self.callbackHelper, 'callback1')
+        clock.sleepUntilNextEvent()
+        self.assertAlmostEqual(clock.now(), start+10, delta=self.DELTA_T)
+        
+        clock.handleEvents(self)
+        self.assertEqual(self.callbackArg, 'callback1')
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+    
