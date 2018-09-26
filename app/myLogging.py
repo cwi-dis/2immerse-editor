8081ff802b834b2f6a7603fed666cdbf53557dbb
Jack Jansen <Jack.Jansen@cwi.nl>
2018-09-26 15:34:50 +0200

    Added flush() method to StreamToLogger and fixed layout

--- app/myLogging.py
+++ app/myLogging.py
@@ -80,14 +80,17 @@ def install(noKibana=False, logLevel=DEFAULT_LOG_CONFIG):
 
 # Send stdout and stderr to the logger as well.
 class StreamToLogger(object):
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
 
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+         
+    def flush(self):
+        pass
 
 sys.stdout = StreamToLogger(logging.getLogger('stdout'), logging.INFO)
 sys.stderr = StreamToLogger(logging.getLogger('stderr'), logging.INFO)
