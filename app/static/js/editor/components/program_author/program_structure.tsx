4e499acd387729d58e035dab346a50bc53c6ec85
Thomas Röggla <t.roggla@cwi.nl>
2018-11-27 17:37:18 +0100

    ProgramStructure is now a stateless functional component

--- app/static/js/editor/components/program_author/program_structure.tsx
+++ app/static/js/editor/components/program_author/program_structure.tsx
@@ -11,41 +11,39 @@ interface ProgramStructureProps {
 const rightTriangle = String.fromCodePoint(9656);
 const downTriangle = String.fromCodePoint(9662);
 
-class ProgramStructure extends React.Component<ProgramStructureProps, {}> {
-  public render() {
-    const { chapters, levelIndent } = this.props;
+const ProgramStructure: React.SFC<ProgramStructureProps> = (props) => {
+  const { chapters, levelIndent } = props;
 
-    const renderTreeLevel = (chapters: List<Chapter>, level = 0): Nullable<JSX.Element> => {
-      if (chapters.isEmpty()) {
-        return null;
-      }
-
-      return (
-        <div style={{marginLeft: (level === 0) ? 8 : levelIndent}}>
-          {chapters.map((chapter, i) => {
-            const name = (chapter.name) ? chapter.name : <span style={{color: "#555555"}}>(to be named)</span>;
-            const bullet = (chapter.children!.isEmpty()) ? rightTriangle : downTriangle;
-
-            return (
-              <div key={i}>
-                <div style={{padding: "3px 0"}}>{bullet} {name}</div>
-                {renderTreeLevel(chapter.children!, level + 1)}
-              </div>
-            );
-          })}
-        </div>
-      );
-    };
+  const renderTreeLevel = (chapters: List<Chapter>, level = 0): Nullable<JSX.Element> => {
+    if (chapters.isEmpty()) {
+      return null;
+    }
 
     return (
-      <div>
-        <div style={{fontWeight: "bold", padding: 8, marginBottom: 8, borderBottom: "2px solid #161616"}}>
-          Structure
-        </div>
-        {renderTreeLevel(chapters)}
+      <div style={{marginLeft: (level === 0) ? 8 : levelIndent}}>
+        {chapters.map((chapter, i) => {
+          const name = (chapter.name) ? chapter.name : <span style={{color: "#555555"}}>(to be named)</span>;
+          const bullet = (chapter.children!.isEmpty()) ? rightTriangle : downTriangle;
+
+          return (
+            <div key={i}>
+              <div style={{padding: "3px 0"}}>{bullet} {name}</div>
+              {renderTreeLevel(chapter.children!, level + 1)}
+            </div>
+          );
+        })}
       </div>
     );
-  }
-}
+  };
+
+  return (
+    <div>
+      <div style={{fontWeight: "bold", padding: 8, marginBottom: 8, borderBottom: "2px solid #161616"}}>
+        Structure
+      </div>
+      {renderTreeLevel(chapters)}
+    </div>
+  );
+};
 
 export default ProgramStructure;
