4035a7ee1c4c4c8eddec5ca5cfd4bce5736edbe0
Thomas Röggla <t.roggla@cwi.nl>
2018-11-27 18:28:32 +0100

    added function to check array equality

--- app/static/js/editor/util.ts
+++ app/static/js/editor/util.ts
@@ -255,6 +255,20 @@ export function getCanvasDropPosition(stageWrapper: Nullable<Stage>, pageX: numb
     ];
 }
 
+export function arraysEqual<T>(a: Array<T>, b: Array<T>): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 type ActionHandlerFunction<T> = (state: T, action: Action) => T;
 
 export class ActionHandler<T> {
