ee1a429108241a76880f33e49bc29cbe56bc4bf2
Thomas Röggla <t.roggla@cwi.nl>
2018-12-07 18:09:51 +0100

    added test cases for document reducer

---
+++ app/static/__tests__/reducers/document.test.ts
@@ -0,0 +1,31 @@
+/// <reference types="jest" />
+
+import reducer, { DocumentState, initialState } from "../../js/editor/reducers/document";
+
+
+describe("Document reducer", () => {
+  it("should return the initial state on an unknown action", () => {
+    expect(
+      reducer(undefined, { type: "" })
+    ).toEqual(initialState);
+  });
+
+  it("should return the given state on an unknown action", () => {
+    const state: DocumentState = {
+      documentId: "helloworld"
+    };
+
+    expect(
+      reducer(state, { type: "" })
+    ).toEqual(state);
+  });
+
+  it("should assign the given document ID on ASSIGN_DOCUMENT_ID", () => {
+    expect(
+      reducer(
+        undefined,
+        { type: "ASSIGN_DOCUMENT_ID", payload: { documentId: "document2" } } as any
+      )
+    ).toEqual({ documentId: "document2" });
+  });
+});
