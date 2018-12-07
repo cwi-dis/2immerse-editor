2de48d2e72bad7d12bfee021eba254e7f90438f0
Thomas Röggla <t.roggla@cwi.nl>
2018-12-07 18:09:35 +0100

    integrated document reducer into reducer chain

--- app/static/js/editor/reducers/index.ts
+++ app/static/js/editor/reducers/index.ts
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 
 import chapters from "./chapters";
+import document from "./document";
 import masters from "./masters";
 import screens from "./screens";
 import timelines from "./timelines";
 
 const rootReducer = combineReducers({
   chapters,
+  document,
   masters,
   router: routerReducer,
   screens,
