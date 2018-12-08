ae370d90fedfe26e264a74e6ff5e913e7360b8d5
Thomas Röggla <tom@lichtfragmente.com>
2018-12-08 17:13:57 +0100

    added action file for dealing with assets

---
+++ app/static/js/editor/actions/assets.ts
@@ -0,0 +1,20 @@
+import { ActionCreatorsMapObject } from "redux";
+import { PayloadAction } from "../util";
+
+export type ADD_ASSET = PayloadAction<"ADD_ASSET", {id: string, name: string, description: string, previewUrl: string, duration?: number}>;
+function addAsset(id: string, name: string, description: string, previewUrl: string, duration?: number): ADD_ASSET {
+  return {
+    type: "ADD_ASSET",
+    payload: {
+      id, name, description, previewUrl, duration
+    }
+  };
+}
+
+export interface DocumentActions extends ActionCreatorsMapObject {
+  addAsset: (id: string, name: string, description: string, previewUrl: string, duration?: number) => ADD_ASSET;
+}
+
+export const actionCreators: DocumentActions = {
+  addAsset
+};
