41f7bb093e66c351c739bfb1f34dd84d403c7757
Thomas Röggla <tom@lichtfragmente.com>
2018-12-09 00:13:56 +0100

    rendering list of assets in DMAppcContainer

--- app/static/js/editor/components/master_manager/dmappc_container.tsx
+++ app/static/js/editor/components/master_manager/dmappc_container.tsx
@@ -11,18 +11,20 @@ class DMAppcContainer extends React.Component<DMAppcContainerProps, {}> {
   }
 
   public render() {
+    const { assets } = this.props;
+
     return (
       <div style={{height: "50%", overflowY: "scroll", backgroundColor: "#353535", borderTop: "1px solid #161616", padding: 2}}>
-        {Array(11).fill(null).map((_, i: number) => {
+        {assets.map((asset, i: number) => {
           return (
             <div
               key={i}
               draggable={true}
-              onDragStart={this.setDragData.bind(this, `component${i}`)}
+              onDragStart={this.setDragData.bind(this, asset.id)}
               style={{backgroundColor: "#262626", margin: 3, width: 140, height: 140, padding: 5, float: "left"}}
             >
-              <b style={{fontSize: 15}}>DMAppC {i}</b>
-              <p style={{marginTop: 10, fontSize: 12}}>This is a short description</p>
+              <b style={{fontSize: 15}}>{asset.name}</b>
+              <p style={{marginTop: 10, fontSize: 12}}>{asset.description}</p>
             </div>
           );
         })}
