00a54899636168da908d862ceeac9b5d74b8ac21
Thomas Röggla <tom@lichtfragmente.com>
2018-12-09 04:15:04 +0100

    ASSIGN_DOCUMENT_ID also takes a base URL now

--- app/static/js/editor/actions/document.ts
+++ app/static/js/editor/actions/document.ts
@@ -1,18 +1,18 @@
 import { ActionCreatorsMapObject } from "redux";
 import { PayloadAction } from "../util";
 
-export type ASSIGN_DOCUMENT_ID = PayloadAction<"ASSIGN_DOCUMENT_ID", {documentId: string}>;
-function assignDocumentId(documentId: string): ASSIGN_DOCUMENT_ID {
+export type ASSIGN_DOCUMENT_ID = PayloadAction<"ASSIGN_DOCUMENT_ID", {documentId: string, baseUrl: string}>;
+function assignDocumentId(documentId: string, baseUrl: string): ASSIGN_DOCUMENT_ID {
   return {
     type: "ASSIGN_DOCUMENT_ID",
     payload: {
-      documentId
+      documentId, baseUrl
     }
   };
 }
 
 export interface DocumentActions extends ActionCreatorsMapObject {
-  assignDocumentId: (documentId: string) => ASSIGN_DOCUMENT_ID;
+  assignDocumentId: (documentId: string, baseUrl: string) => ASSIGN_DOCUMENT_ID;
 }
 
 export const actionCreators: DocumentActions = {
