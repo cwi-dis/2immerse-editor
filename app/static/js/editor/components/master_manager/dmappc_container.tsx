5c28a60d4b14c7e3650233861402e7fa0fba9691
Thomas Röggla <tom@lichtfragmente.com>
2018-12-23 19:00:13 +0100

    DMAppcContainer is now a stateless functional component

--- app/static/js/editor/components/master_manager/dmappc_container.tsx
+++ app/static/js/editor/components/master_manager/dmappc_container.tsx
@@ -6,39 +6,37 @@ interface DMAppcContainerProps {
   assets: AssetState;
 }
 
-class DMAppcContainer extends React.Component<DMAppcContainerProps, {}> {
-  public setDragData(assetId: string, e: React.DragEvent<HTMLDivElement>) {
+const DMAppcContainer: React.SFC<DMAppcContainerProps> = (props) => {
+  const { assets, baseUrl } = props;
+
+  const setDragData = (assetId: string, e: React.DragEvent<HTMLDivElement>) => {
     e.dataTransfer.setData("text/plain", assetId);
-  }
+  };
 
-  public render() {
-    const { assets, baseUrl } = this.props;
+  return (
+    <div style={{height: "50%", overflowY: "scroll", backgroundColor: "#353535", borderTop: "1px solid #161616", padding: 2}}>
+      {assets.map((asset, i: number) => {
+        const previewUrl = baseUrl + asset.previewUrl;
 
-    return (
-      <div style={{height: "50%", overflowY: "scroll", backgroundColor: "#353535", borderTop: "1px solid #161616", padding: 2}}>
-        {assets.map((asset, i: number) => {
-          const previewUrl = baseUrl + asset.previewUrl;
-
-          return (
-            <div
-              key={i}
-              draggable={true}
-              onDragStart={this.setDragData.bind(this, asset.id)}
-              style={{backgroundColor: "#262626", margin: 3, height: 100, padding: 10, display: "flex"}}
-            >
-              <div style={{height: 80, width: 80}}>
-                <img src={previewUrl} style={{ width: 80, maxHeight: 80 }} />
-              </div>
-              <div style={{marginLeft: 15}}>
-                <b style={{fontSize: 15}}>{asset.name}</b>
-                <p style={{marginTop: 10, fontSize: 12}}>{asset.description}</p>
-              </div>
+        return (
+          <div
+            key={i}
+            draggable={true}
+            onDragStart={setDragData.bind(null, asset.id)}
+            style={{backgroundColor: "#262626", margin: 3, height: 100, padding: 10, display: "flex"}}
+          >
+            <div style={{height: 80, width: 80}}>
+              <img src={previewUrl} style={{ width: 80, maxHeight: 80 }} />
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+            <div style={{marginLeft: 15}}>
+              <b style={{fontSize: 15}}>{asset.name}</b>
+              <p style={{marginTop: 10, fontSize: 12}}>{asset.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default DMAppcContainer;
