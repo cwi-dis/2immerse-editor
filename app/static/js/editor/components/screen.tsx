f5b9e64dfda94bdd26637f91e16fa8bfaba17b91
Thomas Röggla <t.roggla@cwi.nl>
2019-01-15 16:57:00 +0100

    added property aspect to DeviceFrame type

--- app/static/js/editor/components/screen.tsx
+++ app/static/js/editor/components/screen.tsx
@@ -17,18 +17,21 @@ interface DeviceFrame {
   url: string;
   screenOffset: [number, number];
   screenSize: [number, number];
+  aspect: number;
 }
 
 const deviceFrames: { [key: string]: DeviceFrame } = {
   "communal": {
     url: "/static/img/tv_frame.png",
     screenOffset: [127 / 1964, 107 / 1366],
-    screenSize: [1708 / 1964, 1144 / 1366]
+    screenSize: [1708 / 1964, 1144 / 1366],
+    aspect: 1964 / 1366
   },
   "personal": {
     url: "/static/img/phone_frame.png",
     screenOffset: [80 / 970, 155 / 1756],
-    screenSize: [805 / 970, 1429 / 1756]
+    screenSize: [805 / 970, 1429 / 1756],
+    aspect: 970 / 1756
   }
 };
 
