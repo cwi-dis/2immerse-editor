2eff57e55c49e44a95e3537f73046ce97dfe705e
Thomas Röggla <tom@lichtfragmente.com>
2019-01-21 14:02:57 +0100

    fixed type issue in asBackButton

--- app/static/js/trigger/utils/back_button.tsx
+++ app/static/js/trigger/utils/back_button.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 
-function asBackButton<P>(LinkComponent: React.ComponentType<P> | string) {
+function asBackButton<P>(WrappedComponent: React.ComponentType<P> | string) {
   // Higher-order component for wrapping a back-arrow in a custom link component
-  return function BackButton<P>(props: P) {
-    return (
-      <LinkComponent className="back-button" {...props}>
-        <span className="icon is-large">
-          <i className="fa fa-arrow-left fas fa-lg" />
-        </span>
-      </LinkComponent>
-    );
+  return class BackButton extends React.Component<P> {
+    render() {
+      return (
+        <WrappedComponent className="back-button" {...this.props}>
+          <span className="icon is-large">
+            <i className="fa fa-arrow-left fas fa-lg" />
+          </span>
+        </WrappedComponent>
+      );
+    }
   };
 }
 
