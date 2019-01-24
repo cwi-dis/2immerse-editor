ed02e5b09017cd7c16625caa17eb04602347ae66
Thomas Röggla <t.roggla@cwi.nl>
2019-01-24 15:31:24 +0100

    documented ActionHandler class

--- app/static/js/editor/action_handler.ts
+++ app/static/js/editor/action_handler.ts
@@ -22,21 +22,50 @@ interface Action {
 
 type ActionHandlerFunction<T> = (state: T, action: Action) => T;
 
+/**
+ * This class represents an alternative to creating reducer functions with big
+ * and unwieldy switch statements. A new instance of this class is initialised
+ * with the initial state for the reducer. Following that, new action handler
+ * functions can be added via the `addHandler()` method, supplying action names
+ * a strings. Finally, a complete reducer function, which can be passed to
+ * `combineReducers()`, can be obtained by calling `getReducer()`.
+ */
 export class ActionHandler<T> {
   private initialState: T;
   private handlers: Map<string, ActionHandlerFunction<T>>;
 
+  /**
+   * Creates a new instance of the class using the given value as default
+   * initial state for the resulting reducer.
+   *
+   * @param initialState Default initial state for the reducer
+   */
   constructor(initialState: T) {
     // Initialise initialState with given object and create empty callback map
     this.initialState = initialState;
     this.handlers = Map();
   }
 
+  /**
+   * Add a new action handler function for the given action name. The function
+   * passed in receives the current state and action with potential payload as
+   * parameters.
+   *
+   * @param action The name of the action corresponding to the reducer function
+   * @param fn The function to be invoked in response to the given action.
+   */
   public addHandler(action: string, fn: ActionHandlerFunction<T>) {
     // Create new entry in handlers map with action name as key and callback as value
     this.handlers = this.handlers.set(action, fn);
   }
 
+  /**
+   * Returns a single reducer function, which is a combination of all handlers
+   * added through `addHandler()`. This resulting function can then be passed
+   * to `combineReducers()`.
+   *
+   * @returns A single reducer function which combines all action handlers into one
+   */
   public getReducer(): ActionHandlerFunction<T> {
     // Return a reducer function which checks the map for the given action name and invokes
     // the associated function to transform the given state or returns the state unchanged
