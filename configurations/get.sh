6f2ae4c96b404a1ee68afc25a7babd34b9fe158c
Thomas Röggla <t.roggla@cwi.nl>
2019-01-25 15:23:23 +0100

    removed obsolete configuration files

--- configurations/get.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-case x$1 in
-x)
-	echo Usage: $0 "http[s]://host:port"
-	exit 1
-	;;
-esac
-url=$1/api/v1/configuration
-curl $url
--- configurations/put.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-case x$1 in
-x)
-	echo Usage: $0 "http[s]://host:port" config.json
-	exit 1
-	;;
-esac
-url=$1/api/v1/configuration
-curl --request PUT --header "Content-Type: application/json" --data @"$2" $url 
