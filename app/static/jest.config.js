686298ebbcf27def3c5d52dd877e2d1db054bf05
Thomas Röggla <t.roggla@cwi.nl>
2019-03-07 17:01:28 +0100

    moved config for jest to separate folder

---
+++ app/static/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*\\.(test|spec))\\.(ts|tsx|js)$",
+  "setupFilesAfterEnv": [
+    "<rootDir>/__tests__/setup.ts"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "json"
+  ]
+}
